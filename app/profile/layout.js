05863c400ddce0e66c7149fa2778cce2edaaaa50
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-07-19 18:24:17 +0330

    create menu

--- app/profile/layout.js
+++ app/profile/layout.js
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import styles from "../../components/modules/profiles/layout/ProfilesLayout.module.css";
 
 export default function ProfileLayout({ children }) {
   return (
-      <div className="p-10 grid grid-cols-4 gap-6">
-        <ul className="border border-slate-200 divide-y  rounded-md col-span-1">
-          <li className="p-2">
-            <Link href="/profile">پروفایل من</Link>
-          </li>
-          <li className="p-2">
-            <Link href="/profile/my-tours">تور ها من</Link>
-          </li>
-          <li className="p-2">
-            <Link href="/profile/transactions">تراکنش ها</Link>
-          </li>
-        </ul>
-        <main className="bg-red-100 col-span-3">{children}</main>
-      </div>
+    <div className={styles.container}>
+      <ul className={styles.profileInformaition}>
+        <li>
+          <Link href="/profile">پروفایل من</Link>
+        </li>
+        <li>
+          <Link href="/profile/my-tours">تور ها من</Link>
+        </li>
+        <li>
+          <Link href="/profile/transactions">تراکنش ها</Link>
+        </li>
+      </ul>
+
+      <main className={styles.profile}>{children}</main>
+    </div>
   );
 }
--- components/modules/header/Header.js
+++ components/modules/header/Header.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
 import LoginModal from "./login/authForm/CheckOTPForm";
 import LoginCodeModal from "./login/authForm/SendOTPForm";
@@ -6,6 +7,9 @@ import HamburgerMenu from "../../icons/HamburgerMenu";
 import styles from "./Header.module.css";
 import Login from "./login/Login";
 import DropDownProfile from "./login/open&removeProfile/DropDownProfile";
+import Link from "next/link";
+// import Torino from "@/components/icons/Torino";
+
 
 function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -13,6 +17,16 @@ function Header() {
   const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
   return (
     <div className={styles.container}>
+      <div className={styles.headerRight}>
+        <Image src="/images/TorinoLogo/Torino.webp" width={146} height={44} alt="Torino Logo"/>
+        <ul className={styles.menu}>
+          <li><Link href="/">صفحه اصلی </Link></li>
+          <li>خدمات گردشگری</li>
+          <li>درباره ما</li>
+          <li>تماس با ما</li>
+        </ul>
+      </div>
+
       <div className={styles.HamburgerMenu}>
         <HamburgerMenu />
       </div>
