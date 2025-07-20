17949f51d7bbae4b7b5568935cf2ecabe345583c
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-07-21 03:02:47 +0330

    Create a list of tours and search for tours

--- templates/HomePage.js
+++ templates/HomePage.js
@@ -1,13 +1,25 @@
+// import TourList from "@/components/modules/tour/tourList/TourList";
+
 import TourList from "@/components/modules/tour/tourList/TourList";
 import styles from "./HomePage.module.css";
 import BannerTorino from "@/components/modules/bannerTorino/BannerTorino";
+// import SearchForm from "@/components/modules/searchForm/SearchForm";
+
 
 function HomePage() {
   return (
     <>
       <BannerTorino />
       <div className={styles.container}>
+        <h3>
+          <span> تورینو</span> برگزار کننده بهترین تور های داخلی و خارجی
+        </h3>
+
+        {/* <SearchForm /> */}
+
+        {/* <TourList /> */}
         <TourList />
+        
       </div>
     </>
   );
