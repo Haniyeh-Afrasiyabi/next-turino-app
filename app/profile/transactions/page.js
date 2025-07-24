331604cbe1b66b45a4183181e155c32b14603713
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-07-24 03:37:35 +0330

    Adding loading and error mode

--- app/profile/transactions/page.js
+++ app/profile/transactions/page.js
@@ -5,31 +5,43 @@ import { userTransactions } from "@/core/services/config";
 
 function UserTransactions() {
   const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
+        setLoading(true);
         const data = await userTransactions();
         setTransactions(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        setError("مشکلی در دریافت تراکنش‌ها پیش آمده");
+      } finally {
+        setLoading(false);
       }
     };
     fetchTransactions();
   }, []);
-
   return (
     <div>
       <h3>transactions</h3>
       <div>
-        {transactions.map((transaction) => (
-          <div key={transaction.id}>
-            <p>{transaction.amount}</p>
-
-            <p>{transaction.createdAt}</p>
-          </div>
-        ))}
+        {loading ? (
+          <p>در حال بارگذاری...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : transactions.length === 0 ? (
+          <p>تراکنشی وجود ندارد</p>
+        ) : (
+          transactions.map((transaction) => (
+            <div key={transaction.id}>
+              <p>{transaction.amount}</p>
+              <p>
+                {new Date(transaction.createdAt).toLocaleDateString("fa-IR")}
+              </p>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
